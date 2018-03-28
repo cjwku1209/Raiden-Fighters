e58acc8b3476077cb049ab16232fc3a4432963c5
Calvin Ku <cjwku@stu.ust.hk>
2018-03-28 11:04:34 +0800

    player movement through pressing keystrokes

--- main.js
+++ main.js
@@ -21,9 +21,49 @@ function gameOver(){
 }
 
 $(document).ready(function() {
+	var x = 0;
+	var y = 0;
 	$('#start-button').click(function() {
 		start();
 		setTimeout(countdown, 1000);
+	});
+	window.addEventListener('keydown', function (e) {
+		console.log(e.keyCode);
+		switch (e.keyCode) {
+			case 37:	//left
+				if(x <= -150){
+					x= -150;
+				}
+				else{
+					x-= 10;
+				}
+				break;
+			case 38://up
+				if(y <= -430){
+					y=-430;
+				}
+				else{
+					y-=10;
+				}
+				break;
+			case 39://right
+				if(x >= 150){
+					x= 150;
+				}
+				else{
+					x+= 10;
+				}
+				break;
+			case 40://down
+				if(y>=0){
+					y=0;
+				}
+				else{
+					y+=10;
+				}
+				break;
+		}
+		$('#player').css('transform', 'translate(' + x +'px,'+ y +'px)');
 	});
 
 });
